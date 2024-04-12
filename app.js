cdd5f8d73103511e118fb31951bd050ac6a7def8
pasinduweerasekara <wmpmweerasekara@gmail.com>
2024-04-13 01:58:13 +0530

    until projects section done

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const hamburgerBtn = document.getElementById("hamburger-btn")
 const menu = document.getElementById("nav-links-container")
+const body = document.getElementById("body")
+const navlinks = document.querySelectorAll(".navlink")
+
 hamburgerBtn.addEventListener('click', ()=>{
     hamburgerBtn.classList.toggle("open")
     menu.classList.toggle("open")
+    body.classList.toggle("noscroll")
 })
+
+navlinks.forEach(element => {
+    element.addEventListener('click',()=>{
+        hamburgerBtn.classList.remove("open")
+        menu.classList.remove("open")
+        body.classList.remove("noscroll")
+    })
+});
